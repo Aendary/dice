500d0aad467cacf82b8f3709a31a4a1c06b1c538
Aendary Elia <aendary.elia@mrjaffesclass.com>
2022-09-20 11:39:47 -0700

    datatypes

---
+++ datatypes-arrays/DataTypes3.java
@@ -0,0 +1,7 @@
+public class DataTypes3 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
---
+++ datatypes-strings/DataTypes1.java
@@ -0,0 +1,10 @@
+public class DataTypes1
+{
+    public static void main(String[] args) 
+    {
+        String string1 = "i am a string";
+        String string2 = string1;
+        String string3 = "i am also a string";
+        boolean stringsequal = string1.equals(string3);
+    }
+}  
